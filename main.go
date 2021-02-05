336e283f3f631faa6efcbb3bf69ae41d0986cb23
dracero <diego.racero@gmail.com>
2021-02-05 15:25:41 -0300

    Update main.go

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"fmt"
+	"os"
 	
-        "github.com/faygun/go-rest-api/helper"
+       // "github.com/faygun/go-rest-api/helper"
 	"github.com/faygun/go-rest-api/models"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 //Connection mongoDB with helper class
@@ -176,4 +181,69 @@ func main() {
 	config := helper.GetConfiguration()
 	log.Fatal(http.ListenAndServe(config.Port, r))
 
+}
+
+// ConnectDB : This is helper function to connect mongoDB
+// If you want to export your function. You must to start upper case function name. Otherwise you won't see your function when you import that on other class.
+func ConnectDB() *mongo.Collection {
+	config := GetConfiguration()
+	// Set client options
+	clientOptions := options.Client().ApplyURI(config.ConnectionString)
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("go_rest_api").Collection("books")
+
+	return collection
+}
+
+// ErrorResponse : This is error model.
+type ErrorResponse struct {
+	StatusCode   int    `json:"status"`
+	ErrorMessage string `json:"message"`
+}
+
+// GetError : This is helper function to prepare error model.
+// If you want to export your function. You must to start upper case function name. Otherwise you won't see your function when you import that on other class.
+func GetError(err error, w http.ResponseWriter) {
+
+	log.Fatal(err.Error())
+	var response = ErrorResponse{
+		ErrorMessage: err.Error(),
+		StatusCode:   http.StatusInternalServerError,
+	}
+
+	message, _ := json.Marshal(response)
+
+	w.WriteHeader(response.StatusCode)
+	w.Write(message)
+}
+
+// Configuration model
+type Configuration struct {
+	Port             string
+	ConnectionString string
+}
+
+// GetConfiguration method basically populate configuration information from .env and return Configuration model
+func GetConfiguration() Configuration {
+	err := godotenv.Load("./.env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	configuration := Configuration{
+		os.Getenv("PORT"),
+		os.Getenv("CONNECTION_STRING"),
+	}
+
+	return configuration
 }
