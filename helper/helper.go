d0959d37396ffd03aab7111dd40d67d997d84079
dracero <diego.racero@gmail.com>
2021-02-05 16:38:27 -0300

    Update helper.go

--- helper/helper.go
+++ helper/helper.go
@@ -71,8 +71,8 @@ func GetConfiguration() Configuration {
 	}*/
 
 	configuration := Configuration{
-		os.Getenv("PORT"),
-		os.Getenv("CONNECTION_STRING"),
+		"4747"//os.Getenv("PORT"),
+		"mongodb+srv://brad123:brad123@cluster0.zf9fl.mongodb.net/go-rest-api?retryWrites=true&w=majority"//os.Getenv("CONNECTION_STRING"),
 	}
 
 	return configuration
