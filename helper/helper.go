975a3b3ccb31b5c0c8fe7effb58af0213d287567
dracero <diego.racero@gmail.com>
2021-02-05 17:45:50 -0300

    Update helper.go

--- helper/helper.go
+++ helper/helper.go
@@ -71,9 +71,20 @@ func GetConfiguration() Configuration {
 	}
 
 	configuration := Configuration{
-		os.Getenv("PORT"),
+		GetPort(),//os.Getenv("PORT"),
 		os.Getenv("CONNECTION_STRING"),
 	}
 
 	return configuration
 }
+
+// Get the Port from the environment so we can run on Heroku
+func GetPort() string {
+var port = os.Getenv("PORT")
+// Set a default port if there is nothing in the environment
+if port == "" {
+		port = "4747"
+ 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+ 	}
+ 	return ":" + port
+ }
