29ff61a3d363d5fbe86a80b8f6f4028418afb659
dracero <diego.racero@gmail.com>
2021-02-05 12:56:15 -0300

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"net/http"
 	
-        "helper/helper"
+        "/helper/helper"
 	"github.com/faygun/go-rest-api/models"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
