e700f6e8a1ff026039a1ac1e43fd585a42c15a69
dracero <diego.racero@gmail.com>
2021-02-06 16:49:19 -0300

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/heroku/go-getting-started/models"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"*/
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 //Connection mongoDB with helper class
