fbe71101d0ccb7d2afe46698052b8f9f5f095af1
dracero <diego.racero@gmail.com>
2021-03-12 12:09:58 -0300

    Update main.go

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ var collection = helper.ConnectDB()
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	// we created Book array
 	var books []models.Book
