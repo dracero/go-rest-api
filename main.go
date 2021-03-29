691750911e1b474573deddc34a682a7bbfe9c58b
dracero <diego.racero@gmail.com>
2021-03-29 17:46:06 -0300

    Update main.go

--- main.go
+++ main.go
@@ -63,8 +63,8 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 func getBook(w http.ResponseWriter, r *http.Request) {
 	// set header.
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	//w.Header().Set("Access-Control-Allow-Origin", "*")
+        //w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	var book models.Book
 	// we get params with mux.
@@ -87,8 +87,8 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 
 func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	//w.Header().Set("Access-Control-Allow-Origin", "*")
+        //w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	var book models.Book
 
@@ -108,8 +108,8 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	//w.Header().Set("Access-Control-Allow-Origin", "*")
+        //w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	var params = mux.Vars(r)
 
@@ -151,8 +151,8 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 func deleteBook(w http.ResponseWriter, r *http.Request) {
 	// Set header
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	//w.Header().Set("Access-Control-Allow-Origin", "*")
+        //w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	// get params
 	var params = mux.Vars(r)
