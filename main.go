a62f2c355783ff99a8176e832cff60dc0abaf598
dracero <diego.racero@gmail.com>
2021-04-07 17:41:30 -0300

    Update main.go

--- main.go
+++ main.go
@@ -151,9 +151,8 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 func deleteBook(w http.ResponseWriter, r *http.Request) {
 	// Set header
 	w.Header().Set("Content-Type", "application/json")
-	
-	//w.Header().Set("Access-Control-Allow-Origin", "*")
-        //w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	// get params
 	var params = mux.Vars(r)
