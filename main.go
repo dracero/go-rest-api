20a3549e999bd56685ecc18e67186a42560e6f28
dracero <diego.racero@gmail.com>
2021-04-11 18:44:05 -0300

    Update main.go

--- main.go
+++ main.go
@@ -107,10 +107,9 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.Header().Set("Access-Control-Allow-Origin", "https://36s21.csb.app")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
 	
 	var params = mux.Vars(r)
 
