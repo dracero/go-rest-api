552ba6d3454e7a889fcf85a15e64294f03e7f3a4
dracero <diego.racero@gmail.com>
2021-04-11 17:30:25 -0300

    Update main.go

--- main.go
+++ main.go
@@ -109,7 +109,8 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 func updateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+        w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+        w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	
 	var params = mux.Vars(r)
 
